4f410c89b924e16f8442cafe6cce5d65b39280e4
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-03 14:28:49 +0600

    product listed

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,18 @@
-/* eslint-disable react/prop-types */
+import React from "react";
+import Product from "./Product";
+
+const Products=({products})=>
+{
+
+    return (
+        <div className="products">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      );
+
+
+};
+
+export default Products;
